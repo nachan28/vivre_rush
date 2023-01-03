dd0a0b5e78c9d0034f813382a6a0b2175ab7549c
nachan28 <torajiro282828@gmail.com>
2023-01-03 10:56:13 +0900

    modified getRandomIdx => getRandomIdxElement()

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,7 @@ const latestEpisode = 1070;
 const syllabary = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
 const categories = {
     懸賞金が: ["低い", "高い", "近い 2", "遠い 2", "低い 男", "高い 男"],
-    名前が: [`50音順で「${getRandomIdx(syllabary)}」に近い`, "長い", "短い"],
+    名前が: [`50音順で「${getRandomIdxElement(syllabary)}」に近い`, "長い", "短い"],
     年齢が: ["高い", "低い", "近い 2", "遠い 2"],
     身長が: ["高い", "低い", "近い 2", "遠い 2"],
     初登場話が: ["早い", "遅い", "近い 2", "遠い 2", `${getRandomNumber(1, latestEpisode)}話に近い`],
@@ -11,7 +11,7 @@ const categories = {
 function getRandomNumber(start, end) {
     return Math.floor(Math.random() * (end - start) + start);
 }
-function getRandomIdx(iterator) {
+function getRandomIdxElement(iterator) {
     const idx = getRandomNumber(0, iterator.length);
     return iterator[idx];
 }
@@ -23,8 +23,8 @@ window.onload = () => {
     document.body.appendChild(subjectList);
 };
 button.addEventListener("click", () => {
-    const currentCategory = getRandomIdx(Object.keys(categories));
-    const currentSubCategory = getRandomIdx(categories[currentCategory]);
+    const currentCategory = getRandomIdxElement(Object.keys(categories));
+    const currentSubCategory = getRandomIdxElement(categories[currentCategory]);
     const subject = document.createElement("li");
     subject.textContent = currentCategory + " " + currentSubCategory + " " + "キャラクターは？";
     subjectList.appendChild(subject);
